bebcbae24394bab3b41c70edfddf5a5efe7ea025
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 03:33:34 +0530

    Index page is now passed into withLayout HOC

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <div>Hello Pusher</div>;
+import withLayout from "../components/Layout";
+
+class Index extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <div>Hello Pusher</div>
+            </div>
+        );
+    }
+}
+
+export default withLayout(Index, "The League Channel");
