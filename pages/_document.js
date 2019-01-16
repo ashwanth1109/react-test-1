94f157c20914d2fc1fa9601b6b9c3348a5b1dd07
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 03:32:44 +0530

    Created document for global, body styling, linking fonts etc.

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+class MyDoc extends Document {
+    render() {
+        return (
+            <html lang="en">
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Coiny"
+                        rel="stylesheet"
+                    />
+                    {/* GLOBAL STYLES GO HERE */}
+                    <style>{`
+                        * {
+                            margin: 0;
+                            padding: 0;
+                        }
+                    `}</style>
+                </Head>
+                <body
+                    style={{
+                        width: "100vw",
+                        height: "100vh",
+                        backgroundColor: "#222",
+                        color: "#fff"
+                    }}
+                >
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
+
+export default MyDoc;
