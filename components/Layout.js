efe461637bb66d97be81407533bff0e4abe2ff72
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 15:19:01 +0530

    Created an updateState function in global dispatchers

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
 import { connect } from "react-redux";
+import { updateState } from "../actions";
 
 const mapStateFromProps = state => {
     return state;
 };
 
-const mapDispatchToProps = dispatch => ({
-    // dispatch actions
-});
+const mapDispatchToProps = dispatch => {
+    return {
+        updateState: (payload, type) => {
+            updateState(dispatch, payload, type);
+        }
+    };
+};
 
 const withLayout = (BaseComponent, pageTitle) => {
     class App extends React.Component {
