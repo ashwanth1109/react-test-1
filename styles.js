8abb0e34c363be72606a53afc76eac46b62faad8
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 13:27:11 +0530

    Created styles js to store commonly used styles

---
+++ styles.js
@@ -0,0 +1,21 @@
+export const fCenter = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+};
+
+export const fScreen = {
+    width: "100vw",
+    height: "100vh"
+};
+
+export const title = {
+    fontFamily: "Coiny, cursive",
+    fontSize: "30px"
+};
+
+export const grid = {
+    display: "grid",
+    gridRowGap: "30px",
+    gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))"
+};
