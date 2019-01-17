67090a36df43326cd82db0e1c30c84aa61b613fb
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 23:20:43 +0530

    Added reducers for room, currentUser, rooms, currentRoom, messages

--- store.js
+++ store.js
@@ -6,7 +6,31 @@ const test = (state = 0, action) =>
 const user = (state = null, action) =>
     action.type === "UPDATE_USER" ? action.payload : state;
 
-export default combineReducers({ test, user });
+const room = (state = null, action) =>
+    action.type === "UPDATE_ROOM" ? action.payload : state;
+
+// i know this seems redundant but i think it helps to not overwrite user reducer
+const currentUser = (state = null, action) =>
+    action.type === "CURRENT_USER" ? action.payload : state;
+
+const rooms = (state = [], action) =>
+    action.type === "ROOMS" ? action.payload : state;
+
+const currentRoom = (state = null, action) =>
+    action.type === "CURRENT_ROOM" ? action.payload : state;
+
+const messages = (state = [], action) =>
+    action.type === "MESSAGES" ? action.payload : state;
+
+export default combineReducers({
+    test,
+    user,
+    room,
+    currentUser,
+    rooms,
+    currentRoom,
+    messages
+});
 
 // ------------------------------------------------------------
 // data store
@@ -15,8 +39,8 @@ export const users = [
     "aquaman",
     "batman",
     "flash",
-    "glantern",
-    "mmanhunter",
+    "green-lantern",
+    "martian-manhunter",
     "superman",
-    "wwoman"
+    "wonder-woman"
 ];
