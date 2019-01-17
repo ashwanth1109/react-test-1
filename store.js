2ab37feb0baef462906544002f5a1a8b598612f2
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 15:17:17 +0530

    Added user reducer

--- store.js
+++ store.js
@@ -3,4 +3,7 @@ import { combineReducers } from "redux";
 const test = (state = 0, action) =>
     action.type === "TEST" ? action.payload : state;
 
-export default combineReducers({ test });
+const user = (state = null, action) =>
+    action.type === "UPDATE_USER" ? action.payload : state;
+
+export default combineReducers({ test, user });
