0a764e499779bfdcea9f4c529d12cf328f697b5e
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-17 23:21:33 +0530

    Chat page now fetches user and room using Chatkit

--- pages/chat.js
+++ pages/chat.js
@@ -1,26 +1,75 @@
 import withLayout from "../components/Layout";
-import axios from "axios";
 import Chatkit from "@pusher/chatkit-client";
 import { users } from "../store";
+import Router from "next/router";
+import { row, bg, col } from "../styles";
+import RoomList from "../components/RoomList";
+import UserList from "../components/UserList";
+
+const s = {
+    container: {
+        height: "100vh",
+        ...row,
+        margin: "0 auto"
+    },
+    userRoomList: {
+        flex: 1,
+        ...col
+    },
+    chatMessages: {
+        flex: 2.5,
+        ...bg.white
+    }
+};
 
 class Chat extends React.Component {
-    state = {
-        chats: []
-    };
+    async componentDidMount({ user, updateState } = this.props) {
+        if (user === null) {
+            Router.push("/");
+        } else {
+            const chatManager = new Chatkit.ChatManager({
+                instanceLocator: process.env.PUSHER_APP_INSTANCE,
+                userId: users[user],
+                tokenProvider: new Chatkit.TokenProvider({
+                    url: process.env.PUSHER_APP_TOKEN
+                })
+            });
 
-    componentDidMount({ user } = this.props) {
-        const chatManager = new Chatkit.ChatManager({
-            instanceLocator: process.env.PUSHER_APP_INSTANCE,
-            userId: users[user]
-        });
+            const currentUser = await chatManager.connect();
+            updateState(currentUser, "CURRENT_USER");
+            const rooms = currentUser.rooms;
+            updateState(rooms, "ROOMS");
+            // const currentRoom = await currentUser.subscribeToRoom({
+            //     roomId: process.env.LEAGUE_ROOM,
+            //     messageLimit: 100,
+            //     hooks: {
+            //         onMessage: message => {
+            //             this.setState({
+            //                 messages: [...messages, message]
+            //             });
+            //         }
+            //     }
+            // });
+            // updateState(currentRoom, "CURRENT_ROOM");
+        }
     }
 
-    render() {
-        return (
-            <div>
-                <div>Chat Page</div>
-            </div>
-        );
+    render({ user } = this.props) {
+        if (user === null) {
+            return null;
+        } else {
+            return (
+                <div style={s.container}>
+                    <div style={s.userRoomList}>
+                        <RoomList />
+                        <UserList />
+                    </div>
+                    <div style={s.chatMessages}>
+                        <div />
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
