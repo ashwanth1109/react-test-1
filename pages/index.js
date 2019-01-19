1c393fbdf40c34237aaa386d63583a61380f73a2
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-19 15:52:54 +0530

    Refactor + Cleanup (Index page)

--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,46 @@
-import withLayout from "../components/Layout";
-import { fCenter, fScreen, title, grid, smallText } from "../styles";
-import Router from "next/router";
-import { users as userIcons } from "../store";
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import withLayout from "../components/Layout"; // Layout Higher Order Component
+import Router from "next/router"; // Next Router
+import { users as userIcons } from "../store"; // user data array
+import { index as s } from "../componentStyles"; // component styles
+import { navigateToChat } from "../utilityFunctions"; // function to navigate to chat page
 
-const s = {
-    container: {
-        ...fScreen,
-        ...fCenter,
-        flexDirection: "column"
-    },
-    info: {
-        ...title
-    },
-    icons: {
-        width: "60%",
-        margin: "50px 0",
-        ...grid
-    },
-    icon: {
-        width: "120px",
-        height: "120px",
-        margin: "0 auto",
-        cursor: "pointer",
-        borderRadius: "60px"
-    },
-    selected: {
-        border: "3px solid #ffffff"
-    },
-    unselected: {
-        border: "3px solid #ffffff00"
-    },
-    button: {
-        padding: "10px 30px",
-        ...fCenter,
-        ...smallText,
-        border: "3px solid #fff",
-        borderRadius: "5px",
-        cursor: "pointer",
-        transition: "0.2s ease-in-out"
-    }
-};
+// ------------------------------------------------------------
+// Index Page Component
+// ------------------------------------------------------------
+const Index = ({ user, updateState }) => (
+    <div style={s.container}>
+        {/* Info Text: Choose your identity */}
+        <div style={s.info}>Choose your identity</div>
+        {/* Display Icons for all users - You can pick one */}
+        <div style={s.icons}>
+            {userIcons.map((userIcon, id) => (
+                <img
+                    key={id}
+                    src={`/static/${userIcon}.png`}
+                    style={{
+                        ...s.icon,
+                        ...(user === id ? s.selected : s.unselected)
+                    }}
+                    alt={userIcon}
+                    onClick={() => updateState(id, "UPDATE_USER")}
+                />
+            ))}
+        </div>
+        {/* If user, clicking this button will navigate to chat page */}
+        <div
+            style={s.button}
+            className="btn"
+            onClick={() => navigateToChat(user, Router)}
+        >
+            Log into the Channel
+        </div>
+    </div>
+);
 
-class Index extends React.Component {
-    navigateToChat = ({ user } = this.props) =>
-        user !== null ? Router.push("/chat") : null;
-
-    render({ user, updateState } = this.props) {
-        return (
-            <div style={s.container}>
-                <div style={s.info}>Choose your identity</div>
-                <div style={s.icons}>
-                    {userIcons.map((userIcon, id) => (
-                        <img
-                            key={id}
-                            src={`/static/${userIcon}.png`}
-                            style={{
-                                ...s.icon,
-                                ...(user === id ? s.selected : s.unselected)
-                            }}
-                            alt={userIcon}
-                            onClick={() => updateState(id, "UPDATE_USER")}
-                        />
-                    ))}
-                </div>
-                <div
-                    style={s.button}
-                    className="btn"
-                    onClick={() => this.navigateToChat()}
-                >
-                    Log into the Channel
-                </div>
-            </div>
-        );
-    }
-}
-
+// ------------------------------------------------------------
+// export with Layout HOC and parameter for Page title
+// ------------------------------------------------------------
 export default withLayout(Index, "Time for Justice");
