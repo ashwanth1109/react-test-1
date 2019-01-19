cf07a1120dc4792f34c7400728aa246cf36b1200
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-19 22:57:55 +0530

    Added Comments to withRedux

--- components/Redux.js
+++ components/Redux.js
@@ -1,9 +1,20 @@
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
 import { connect } from "react-redux";
 import { mapStateFromProps, mapDispatchToProps } from "../reduxMapFunctions";
-
+// ------------------------------------------------------------
+// withRedux Higher Order Component
+// ------------------------------------------------------------
 const withRedux = (BaseComponent, withMapState, withMapDispatch) => {
+    // ------------------------------------------------------------
+    // create App Component
+    // ------------------------------------------------------------
     const App = props => <BaseComponent {...props} />;
-
+    // ------------------------------------------------------------
+    // Check if passed in Component needs access to both mapState and mapDispatch
+    // or only one of them
+    // ------------------------------------------------------------
     if (withMapState && withMapDispatch) {
         return connect(
             mapStateFromProps,
@@ -24,5 +35,7 @@ const withRedux = (BaseComponent, withMapState, withMapDispatch) => {
         return App;
     }
 };
-
+// ------------------------------------------------------------
+// export HOC function
+// ------------------------------------------------------------
 export default withRedux;
