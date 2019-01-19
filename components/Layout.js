b54c184c6eb336f7cd2a587001fa0038c426f0c4
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-19 22:58:39 +0530

    Refactor + Comments - withLayout HOC

--- components/Layout.js
+++ components/Layout.js
@@ -1,42 +1,34 @@
-import Head from "next/head";
-import { connect } from "react-redux";
-import { updateState } from "../store";
-import NProgress from "nprogress";
-import Router from "next/router";
-
-const mapStateFromProps = state => {
-    console.log(state);
-    return state;
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        updateState: (payload, type) => {
-            updateState(dispatch, payload, type);
-        }
-    };
-};
-
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import Head from "next/head"; // Overwrite Head from Next JS
+import NProgress from "nprogress"; // Set nprogress loader on route change
+import Router from "next/router"; // Router from Next JS
+import withRedux from "./Redux"; // with Redux HOC
+// ------------------------------------------------------------
+// Configure route change methods to nprogress methods
+// ------------------------------------------------------------
 Router.onRouteChangeStart = () => NProgress.start();
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.error();
-
+// ------------------------------------------------------------
+// withLayout Higher Order Component
+// ------------------------------------------------------------
 const withLayout = (BaseComponent, pageTitle) => {
-    class App extends React.Component {
-        render = ({ props } = this) => (
-            <React.Fragment>
-                <Head>
-                    <title>{pageTitle || "Realtime Chat"}</title>
-                </Head>
-                <BaseComponent {...props} />
-            </React.Fragment>
-        );
-    }
-
-    return connect(
-        mapStateFromProps,
-        mapDispatchToProps
-    )(App);
+    const App = props => (
+        <React.Fragment>
+            <Head>
+                <title>{pageTitle || "Realtime Chat"}</title>
+            </Head>
+            <BaseComponent {...props} />
+        </React.Fragment>
+    );
+    // ------------------------------------------------------------
+    // return App component withRedux HOC
+    // ------------------------------------------------------------
+    return withRedux(App, true, true);
 };
-
+// ------------------------------------------------------------
+// return withLayout HOC method
+// ------------------------------------------------------------
 export default withLayout;
