406cf593c12379b1e3ed56757b42df42edec956e
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-19 19:13:08 +0530

    Cleanup + Comments - ChatMessages

--- components/ChatMessages.js
+++ components/ChatMessages.js
@@ -1,55 +1,35 @@
-import React from "react";
-
-import { connect } from "react-redux";
-import Message from "./Message";
-import { col } from "../styles";
-
-import { AutoSizer, List } from "react-virtualized";
-
-const mapStateFromProps = state => {
-    return {
-        messages: state.messages
-    };
-};
-
-const s = {
-    container: {
-        flex: "1",
-        backgroundColor: "#666",
-        ...col,
-        padding: "20px 0"
-    }
-};
-
-class ChatMessages extends React.Component {
-    rowRenderer = ({ key, index, style }, { messages } = this.props) => {
-        return (
-            <div key={key} style={style}>
-                <Message message={messages[index]} />
-            </div>
-        );
-    };
-
-    render({ messages } = this.props) {
-        console.log(messages.length);
-        return (
-            <div style={s.container}>
-                <AutoSizer>
-                    {({ height, width }) => (
-                        <List
-                            ref="List"
-                            width={width}
-                            height={height}
-                            rowCount={messages.length}
-                            rowHeight={143}
-                            rowRenderer={this.rowRenderer}
-                            scrollToIndex={messages.length - 1}
-                        />
-                    )}
-                </AutoSizer>
-            </div>
-        );
-    }
-}
-
-export default connect(mapStateFromProps)(ChatMessages);
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import withRedux from "./Redux"; // withRedux HOC
+import { chatMessages as s } from "../componentStyles"; // component styles
+import { AutoSizer, List } from "react-virtualized"; // React Virtualized List
+import { renderChatMessages } from "../utilityFunctions";
+// ------------------------------------------------------------
+// Chat Messages Component
+// ------------------------------------------------------------
+const ChatMessages = ({ messages }) => (
+    <div style={s.container}>
+        {/* Auto Sizer occupies full size available to render virtualized list */}
+        <AutoSizer>
+            {/* react virtualized list */}
+            {({ height, width }) => (
+                <List
+                    width={width}
+                    height={height}
+                    rowCount={messages.length}
+                    rowHeight={143}
+                    rowRenderer={rowRendererProps =>
+                        renderChatMessages(rowRendererProps, messages)
+                    }
+                    scrollToIndex={messages.length - 1}
+                    style={{ outline: "none" }}
+                />
+            )}
+        </AutoSizer>
+    </div>
+);
+// ------------------------------------------------------------
+// export with redux HOC - only mapState required
+// ------------------------------------------------------------
+export default withRedux(ChatMessages, true, false);
