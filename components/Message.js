db04a8319447a86cc0bcfac1af90ff2fdc221ce6
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-19 22:58:22 +0530

    Refactored Message component

--- components/Message.js
+++ components/Message.js
@@ -1,60 +1,28 @@
-import React from "react";
-import { col, smallText } from "../styles";
-
-const s = {
-    container: {
-        width: "100%",
-        padding: "50px 10px 0 40px",
-        boxSizing: "border-box",
-        ...col
-    },
-    message: {
-        width: "100%",
-        backgroundColor: "#222",
-        borderRadius: "10px",
-        position: "relative"
-    },
-    image: {
-        position: "absolute",
-        width: "80px",
-        height: "80px",
-        borderRadius: "40px",
-        top: "-40px",
-        left: "-20px",
-        border: "2px solid #fff"
-    },
-    messageBody: {
-        padding: "10px 10px 20px 70px",
-        fontSize: "20px"
-    }
-};
-
-class Message extends React.Component {
-    render({ message } = this.props) {
-        console.log(message.senderId);
-        return (
-            <div style={s.container}>
-                <div style={s.message}>
-                    <div
-                        style={{
-                            width: "100%",
-                            height: "40px",
-                            ...smallText,
-                            padding: "0 0 0 70px"
-                        }}
-                    >
-                        {message.senderId.toUpperCase()}
-                    </div>
-                    <div style={s.messageBody}>{message.text}</div>
-                    <img
-                        src={`/static/${message.senderId}.png`}
-                        style={s.image}
-                        alt={message.senderId}
-                    />
-                </div>
-            </div>
-        );
-    }
-}
-
-export default Message;
+// ------------------------------------------------------------
+// import dependencies
+// ------------------------------------------------------------
+import { message as s } from "../componentStyles"; // component styles
+// ------------------------------------------------------------
+// Message component
+// ------------------------------------------------------------
+const Message = ({ message }) => (
+    <div style={s.container}>
+        {/* messsage chat box container */}
+        <div style={s.message}>
+            {/* display message sender */}
+            <div style={s.messageSender}>{message.senderId.toUpperCase()}</div>
+            {/* display the content of message */}
+            <div style={s.messageBody}>{message.text}</div>
+            {/* display image of sender */}
+            <img
+                src={`/static/${message.senderId}.png`}
+                style={s.image}
+                alt={message.senderId}
+            />
+        </div>
+    </div>
+);
+// ------------------------------------------------------------
+// export message as memoized component
+// ------------------------------------------------------------
+export default React.memo(Message);
